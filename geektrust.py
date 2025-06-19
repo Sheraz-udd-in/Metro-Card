12ec94b2a8a0720e79d27331320462541ed5d832
abhijeet <20ucc003@lnmiit.ac.in>
2025-06-19 11:10:36 +0530

    metro card assigment test case working

---
+++ geektrust.py
@@ -0,0 +1,27 @@
+from sys import argv
+from src import service
+def main():
+    
+    """
+    Sample code to read inputs from the file
+
+    if len(argv) != 2:
+        raise Exception("File path not entered")
+    file_path = argv[1]
+    f = open(file_path, 'r')
+    Lines = f.readlines()
+    //Add your code here to process the input commands
+    """
+    with open("./sample_input/input1.txt" ,  "r") as file  :
+        for line in file  :
+            arr =   line.split()
+            if arr[0] == "BALANCE" :
+                service.balance(arr[1] , arr[2])
+            if arr[0] == "CHECK_IN" :
+                service.check_in(arr[1] , arr[2] , arr[3])
+            if arr[0] == "PRINT_SUMMARY" :
+                service.summary()
+
+    
+if __name__ == "__main__":
+    main()
---
+++ src/service.py
@@ -0,0 +1,46 @@
+from  .repository import *
+from  .model import MetroCard
+
+def balance(mid  ,  balance ):
+    metroCard[mid] = MetroCard(mid  ,int(balance) )
+
+
+
+def rechargeCard(card  , ammount , src) :
+    card.add_balance(ammount)
+    station  = stations[src]
+    x =  ammount*2/100
+    station.add_ammount(x)
+
+
+def check_in(mid  ,  type  ,  src) :
+    card = metroCard[mid]
+    fare =  rates[type]
+    station = stations[src]
+
+    if (card.src == "AIRPORT" and src == "CENTRAL") or (card.src == "CENTRAL" and src == "AIRPORT") :
+        fare =  fare/2
+        station.add_discount(fare)
+
+    if card.balance <  fare  :
+        rechargeCard(card , fare - card.balance , src)
+
+    card.add_balance(-1*fare)
+    card.update_src(src)
+
+
+    station.add_ammount(fare)
+    station.add_passenger(type)
+
+
+def summary() :
+    central  =  stations['CENTRAL']
+    print(central.total_ammount)
+    print(central.discount)
+    print(central.passengerHistory)
+    airport  =  stations['AIRPORT']
+    print(airport.total_ammount)
+    print(airport.discount)
+    print(airport.passengerHistory)
+
+
